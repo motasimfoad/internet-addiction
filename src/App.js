d5ab65811bdb2bdca6303f13e0deae0173e51701
Motasim Foad <motasimfoad@gmail.com>
2020-07-13 22:31:40 +0600

    Added bootstraps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <Button>
+          OMG
+        </Button>
       </header>
     </div>
   );
