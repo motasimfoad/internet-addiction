8c5855318418954ff85744cce5c017e9808ca4a7
Motasim Foad <motasimfoad@gmail.com>
2020-08-21 01:27:13 +0600

    added split screen function

--- src/Screen/Home/index.js
+++ src/Screen/Home/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import QuizWizard from '../../Component/QuizWizard'
+import { First } from 'react-bootstrap/esm/PageItem';
 
 function App() {
 
+  const [screen, setScreen] = useState("first"); 
+
   return (
     <div className="App">
-      <QuizWizard />
+      {screen === "first" ? (
+        <div>
+        <h1>
+          bla bla bla bla bla
+        </h1>
+        <button onClick={()=>setScreen("second")}>Agreed</button>
+      </div>
+      ) : (
+        <QuizWizard />
+      )}
     </div>
   );
 }
