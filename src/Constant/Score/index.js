b217985f7047f24c6a239e47dda0cc349af7dc3e
Motasim Foad <motasimfoad@gmail.com>
2020-08-22 23:15:05 +0600

    Small fix on scoreText

--- src/Constant/Score/index.js
+++ src/Constant/Score/index.js
@@ -1,6 +1,6 @@
 const scoreText = [
     "You are an average on-line user. You may surf the Web a bit too long at times, but you have control over your usage.",
-    "You are experiencing occasional or frequent problems because of the Internet. You should consider their full impacton your life.",
+    "You are experiencing occasional or frequent problems because of the Internet. You should consider their full impact on your life.",
     "Your Internet usage is causing significant problems in your life. You should elevate the impact of the Internet on your life and address the problems directly caused by your Internet usage.",
 ];
 
