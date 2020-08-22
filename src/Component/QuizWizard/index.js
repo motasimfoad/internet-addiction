b9b8d5cea8f307b82c25915e42d58da4b2669144
Motasim Foad <motasimfoad@gmail.com>
2020-08-22 17:46:28 +0600

    quizwazard card added

--- src/Component/QuizWizard/index.js
+++ src/Component/QuizWizard/index.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import questionList from '../../Constant/Question';
 import ScoreWizard from '../ScoreWizard';
-import {Row, Button, Col, Card} from 'react-bootstrap';
+import {Button, Col, Card} from 'react-bootstrap';
 
 function Wizard(info) {
 
@@ -20,19 +20,27 @@ function pointKeepr(point) {
     <Col className="container">
       {questionCounter <= question.length-1 ? (
         <div className="vertical-center">
-            {question[questionCounter]} <br />
-            {questionCounter} out of {question.length} <br />
-            <div>
-                <Button onClick={()=>pointKeepr(0)}>0</Button>
-                <Button onClick={()=>pointKeepr(1)}>1</Button>
-                <Button onClick={()=>pointKeepr(2)}>2</Button>
-                <Button onClick={()=>pointKeepr(3)}>3</Button>
-                <Button onClick={()=>pointKeepr(4)}>4</Button>
-                <Button onClick={()=>pointKeepr(5)}>5</Button>
-            </div>
+          <Card>
+            <Card.Body>
+              <Card.Subtitle style={{textAlign:"right"}}>
+                {questionCounter} out of {question.length} 
+              </Card.Subtitle>
+              <Card.Text>
+                <h5>
+                {question[questionCounter]}
+                </h5>
+              </Card.Text>
+                <div>
+                  <Button variant="outline-dark" onClick={()=>pointKeepr(0)}>0</Button>
+                  <Button variant="outline-dark" onClick={()=>pointKeepr(1)}>1</Button>
+                  <Button variant="outline-dark" onClick={()=>pointKeepr(2)}>2</Button>
+                  <Button variant="outline-dark" onClick={()=>pointKeepr(3)}>3</Button>
+                  <Button variant="outline-dark" onClick={()=>pointKeepr(4)}>4</Button>
+                  <Button variant="outline-dark" onClick={()=>pointKeepr(5)}>5</Button>
+                </div>
+            </Card.Body>
+          </Card>
         </div>
-       
-       
       ) : (
         <ScoreWizard score={score}/>
       )}
