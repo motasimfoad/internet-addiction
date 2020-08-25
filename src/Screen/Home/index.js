3b83b79874ff18fbcdc44abc04097551481bd7a8
Motasim Foad <motasimfoad@gmail.com>
2020-08-25 21:05:37 +0600

    ReactGA bug fixed

--- src/Screen/Home/index.js
+++ src/Screen/Home/index.js
@@ -5,12 +5,13 @@ import '../Home/style.css';
 import Footer from '../../Component/Footer';
 import ReactGa from 'react-ga';
 
-function App() {
 
 // Google Analytics for Motasim Foad
-  ReactGa.initialize("UA-154721739-1");
-  ReactGa.pageview('Motasim Foads IAT');
+ReactGa.initialize("UA-154721739-1");
+ReactGa.pageview('Motasim Foads IAT');
 // Google Analytics for Motasim Foad
+
+function App() {
 
   const [screen, setScreen] = useState("first"); 
   const [name, setName] = useState("No Name"); 
